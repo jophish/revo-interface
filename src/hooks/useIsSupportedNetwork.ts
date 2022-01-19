2747439442f766d9455cc0e2fb91a0ec81fd2b9d
cajubelt <cajubelt@users.noreply.github.com>
2022-01-19 04:00:49 +0000

    Deploying to main from @ revo-market/revo-interface@bd855beb2a897044b0c733637ae87b63d42b0bc7 🚀

--- src/hooks/useIsSupportedNetwork.ts
+++
@@ -1,7 +0,0 @@
-import { ChainId, useContractKit } from '@celo-tools/use-contractkit'
-
-export const useIsSupportedNetwork = () => {
-  const { network } = useContractKit()
-
-  return [ChainId.CeloMainnet, ChainId.Alfajores].includes(network.chainId)
-}
