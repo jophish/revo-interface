69aa70063954138030a7a3454f7a764805d7fb4e
Charlie Andrews <charlie.andrews@valoraapp.com>
2022-01-18 20:45:15 -0800

    Revert "Deploying to main from @ revo-market/revo-interface@bd855beb2a897044b0c733637ae87b63d42b0bc7 🚀"
    This reverts commit 2747439442f766d9455cc0e2fb91a0ec81fd2b9d.

---
+++ src/state/stake/useOwnerStakedPools.ts
@@ -0,0 +1,47 @@
+import { useContractKit } from '@celo-tools/use-contractkit'
+import { Interface } from '@ethersproject/abi'
+import { partition } from 'lodash'
+import { FarmSummary } from 'pages/Earn/useFarmRegistry'
+import { useMemo } from 'react'
+import { useMultipleContractSingleData } from 'state/multicall/hooks'
+import { toBN } from 'web3-utils'
+
+import DUAL_REWARDS_ABI from '../../constants/abis/moola/MoolaStakingRewards.json'
+
+// get all staked pools
+export const useOwnerStakedPools = (farmSummaries: FarmSummary[]) => {
+  const { address: owner } = useContractKit()
+
+  const data = useMultipleContractSingleData(
+    farmSummaries.map((farmSummaries) => farmSummaries.stakingAddress),
+    new Interface(DUAL_REWARDS_ABI),
+    'balanceOf',
+    [owner || undefined]
+  )
+
+  const isStaked: Record<string, boolean> = data.reduce<Record<string, boolean>>((acc, curr, idx) => {
+    acc[farmSummaries[idx].stakingAddress] = curr?.result?.[0].gt('0')
+    return acc
+  }, {})
+
+  const [stakedFarms, uniqueUnstakedFarms] = useMemo(() => {
+    const [staked, unstaked] = partition(farmSummaries, (farmSummary) => isStaked[farmSummary.stakingAddress])
+    return [staked, unique(unstaked)]
+  }, [farmSummaries, isStaked])
+
+  return { stakedFarms, unstakedFarms: uniqueUnstakedFarms }
+}
+
+function unique(farmSummaries: FarmSummary[]): FarmSummary[] {
+  const bestFarms: Record<string, FarmSummary> = {}
+  farmSummaries.forEach((fs) => {
+    if (!bestFarms[fs.lpAddress]) {
+      bestFarms[fs.lpAddress] = fs
+    }
+    const currentBest = bestFarms[fs.lpAddress]
+    if (toBN(fs.rewardsUSDPerYear).gt(toBN(currentBest.rewardsUSDPerYear))) {
+      bestFarms[fs.lpAddress] = fs
+    }
+  })
+  return Object.values(bestFarms)
+}
